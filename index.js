b2356db8361795bfdd4fca87f8335969c18e0e02
Theophilus E.K Adukpo <theoadu@gmail.com>
2021-07-30 10:51:56 +0000

    Add commander options

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const program = require('commander');
+const program = require("commander");
 
-program.version('1.0.0').description('Simple Password Generator').parse()
+program.version("1.0.0").description("Simple Password Generator");
+
+program
+    .option("-l, --length <number>", "length of password", "8")
+    .option("-s, --save", "save password to password.txt")
+    .option("-nn, --no-numbers", "remove numbers")
+    .option("-ns, --no-symbols", "remove symbols")
+    .parse();
+
+const {length, save, numbers, symbols} = program.opts()
+
+console.log(program.opts());
